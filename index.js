0c6ca2d21f37a88e65badde83e783154e9f2a27d
Haukur Rosinkranz <hauxir@gmail.com>
2020-04-28 01:27:14 +0200

    update

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 const puppeteer = require("puppeteer");
-const argv = require('minimist')(process.argv.slice(2));
-
-
-let qstr = "";
-if(argv.map)
-    qstr = qstr + "&map=" + argv.map;
-if(argv.game_type)
-    qstr = qstr + "&game_type=" + argv.game_type;
-if(argv.game_mode)
-    qstr = qstr + "&game_mode=" + argv.game_mode;
-
+const argv = require("minimist")(process.argv.slice(2));
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
       "--disable-background-timer-throttling",
-      "--disable-renderer-backgrounding"
-    ]
+      "--disable-renderer-backgrounding",
+    ],
   });
   const page = await browser.newPage();
 
-  await page.goto(
-    "https://openarena.live/" +
-      Math.random()
-        .toString(36)
-        .substring(7) +
-      "?dedicated" + qstr
-  );
-  page.on("console", msg => console.log(msg._text));
-  await page.waitForSelector(".quake_container");
+  await page.goto("https://app.kosmi.io");
   await page.evaluate(() => {
-    const btn = document.querySelector(".quake_container button");
+    localStorage.setItem("token", argv.token);
+  });
+  await page.goto("https://app.kosmi.io/room/" + argv.room_id);
+  await page.evaluate(() => {
+    window.isHeadless = true;
+  });
+  page.on("console", (msg) => console.log(msg._text));
+  await page.waitForSelector("#start_server_btn-Okb");
+  await page.evaluate(() => {
+    const btn = [...document.querySelectorAll("button")].filter(
+      (b) => b.innerText === "Start Server"
+    )[0];
     btn.click();
   });
 })();
